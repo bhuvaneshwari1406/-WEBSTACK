a8594b6793caf15fa2bb262bd277b12a61599447
bhuvaneshwari1406 <82176162+bhuvaneshwari1406@users.noreply.github.com>
2021-04-10 11:42:40 +0530

    Add files via upload

---
+++ full stack course/Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\ki127756.inf_amd64_cd9f19051b5853c8\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\ki127756.inf_amd64_cd9f19051b5853c8\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
